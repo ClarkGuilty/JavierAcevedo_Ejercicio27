f82291802cb886ca08c00f6983999301c843f32e
Javier Alejandro Acevedo Barroso <ja.acevedo12@uniandes.edu.co>
2018-11-14 07:47:28 -0500

    to Cluster

--- plots.py
+++ plots.py
@@ -9,18 +9,17 @@ Created on Sun Nov  4 00:33:14 2018
 
 import numpy as np
 import matplotlib
-matploblib.use("Agg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-def gaussiana(x,mu,sigma):
-    return (1/(sigma*np.sqrt(2*np.pi)))*np.exp(-0.5*((x-mu)/sigma)**2)
 
+dat1 = np.loadtxt("sample_1.dat")
+n = dat1[0]
+dat1 = dat1[1:]
+print(len(dat1))
+#x = np.linspace(constantes[1]-3.5*constantes[2],constantes[1]+3.5*constantes[2],int(constantes[0]))
 
-constantes = np.loadtxt("stan.dat")
-puntos = np.loadtxt("sample.dat")
-x = np.linspace(constantes[1]-3.5*constantes[2],constantes[1]+3.5*constantes[2],int(constantes[0]))
-
-plt.plot(x,gaussiana(x,constantes[1],constantes[2]))
-plt.hist(puntos,bins=int(constantes[0]//12),density=True)
-plt.savefig("sample.pdf")
+#plt.plot(x,gaussiana(x,constantes[1],constantes[2]))
+#plt.hist(puntos,bins=int(constantes[0]//12),density=True)
+#plt.savefig("sample.pdf")
 
