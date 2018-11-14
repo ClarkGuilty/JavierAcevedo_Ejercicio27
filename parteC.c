e7ab620d89d194083a438c190cad1bdf8a3e835f
Javier Alejandro Acevedo Barroso <ja.acevedo12@uniandes.edu.co>
2018-11-14 07:21:17 -0500

    qué razón hay para estar en el salón?

---
+++ parteC.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "mpi.h"
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.141592653589793
+int main (int argc, char *argv[]){
+int rank, size, i;
+double u1, u2, actual;
+number =0;
+if( argc != 4 ) {
+    printf("La entrada debe ser de la forma %s N mu sigma\n", argv[0]);
+    exit(0);
+}
+char *filename= (char*) malloc(200* sizeof(char));
+srand(time(NULL));   
+int N = atoi(argv[1]);
+int mu = atoi(argv[2]);
+int sigma = atoi(argv[3]);
+printf("N=%d\n",N);
+/* starts MPI */
+MPI_Init (&argc, &argv);
+/* get current process rank */
+MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+MPI_Comm_size (MPI_COMM_WORLD, &size);
+sprintf(filename, "sample_%d.dat", rank+1);
+FILE *output = fopen(filename, "w+");
+double *arreglo = malloc(N/size*sizeof(double));
+for(i=rank*N/size;i<(rank+1)*N/size/2;i++){
+    u1 = rand()/(RAND_MAX*1.0);
+    u2 = rand()/(RAND_MAX*1.0);
+    actual = sqrt(-2*log(u1))*sin(2*PI*u2);
+    fprintf(output, "%f\n",actual*sigma+mu); 
+}
+
+MPI_Finalize(); /* ends MPI */
+return 0;
+}
